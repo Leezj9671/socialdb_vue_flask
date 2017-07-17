bd59605399a48f25341cedbb56dcacb0ede55687
lizhengjun <leezj9671@gmail.com>
2017-07-18 00:48:50 +0800

    Echarts tooltip append
    nicer & fix some bugs

--- frontend/src/components/Analysis.vue
+++ frontend/src/components/Analysis.vue
@@ -2,58 +2,30 @@
     <div class="analysis">
         <h2>Database Analysis</h2>
         <div class="button-group">
-            <button v-for="key in analysisItems" v-bind:value="key.value" @click="flashAnalysis(key.value)" type="button" class="button button-pill button-action">
+            <button v-for="key in analysisItems" v-bind:value="key.value" @click="flashAnalysis(key.value, key.text)" type="button" class="button button-pill button-action">
                 {{ key.text}}
             </button>
         </div>
-        <div id="chartArea" style="margin: 0 auto; width: 600px; height:400px;"></div>
+        <div id="chartArea" style="margin: 0 auto; width: 700px; height:400px;"></div>
     </div>
 </template>
 
 
 <script>
+// 改为CDN引入
 // import axios from 'axios'
-// import echarts from '//cdn.bootcss.com/echarts/3.2.2/echarts.simple.min.js'
 // var echarts = require('echarts/lib/echarts');
 // require('echarts/lib/chart/pie')
 // require('echarts/lib/component/tooltip');
 
-axios.defaults.baseURL = 'http://123.207.89.91:5000/api';
 var myChart = null;
-
-// var options = ({
-//     backgroundColor: 'white',
-
-//     tooltip : {
-//         trigger: 'item',
-//         formatter: "{a} {b}: {c} ({d}%)"
-//     },
-
-//     visualMap: {
-//         show: true,
-//         min: 80,
-//         max: 600,
-//         inRange: {
-//             colorLightness: [0, 1]
-//         }
-//     },
-//     series: [
-//         {
-//             name:'来源',
-//             type:'pie',
-//             radius : '55%',
-//             center: ['50%', '50%'],
-//             data:[],
-//             roseType: 'radius',
-//         }
-//     ]
-// })
 
 export default {
   name:'analysis',
   data () {
     return {
         analysisData: [],
+        legendData: [],
         analysisItems: [
             {text: '来源', value: 'source'},
             {text: '泄露时间', value: 'xtime'},
@@ -62,43 +34,51 @@ export default {
     }
   },
   methods:{
-    flashAnalysis: function (value) {
-        myChart = echarts.init(document.getElementById('chartArea'));
+    flashAnalysis: function (value, vtext) {
         myChart.showLoading({
             text: "数据获取ing...",
         });
         axios.get('/analysis/'+value).then(response => {
             if(response.data.status === 'ok'){
                 this.analysisData = [];
-                for (var key in response.data.data) {
+                this.legendData = [];
+                for (var key in response.data.data) {   
                     var item = response.data.data[key];
                     this.analysisData.push({
                         "value": item["sum"],
                         "name": item["_id"]
-                    })
+                    });
+                    this.legendData.push(item["_id"]);
                 }
                 myChart.hideLoading();
                 myChart.setOption({
-                    tootip: {
-                        show: true,
+                    title: {
+                        text: vtext,
+                        x: 'center'
+                    },
+                    tooltip : {
                         trigger: 'item',
-                        formatter: "{a} {b}: {c} ({d}%)"
+                        formatter: "{a} <br/>{b} : {c} ({d}%)"
+                    },
+                    legend: {
+                        orient: 'vertical',
+                        left: 'left',
+                        data: this.legendData
                     },
                     series: {
-                        name: '来源',
+                        name: vtext,
                         type: 'pie',
                         data: this.analysisData
                     }
                 });
-
             }
         })
         .catch(error => { console.log(error); })
         }
     },
-    // mounted(){
-    //     myChart = echarts.init(document.getElementById('chartArea'));
-    // }
+    mounted(){
+        myChart = echarts.init(document.getElementById('chartArea'));
+    }
 }
 </script>
 
